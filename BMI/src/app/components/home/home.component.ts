5d7bec66455d83725ba92c4e2502e3fd2c736ff2
Gitesh Sharma <gitesh15796@gmail.com>
2022-08-10 19:49:35 +0530

    form modified -user can select kg/lbs & cm/inch

--- BMI/src/app/components/home/home.component.ts
+++ BMI/src/app/components/home/home.component.ts
@@ -28,8 +28,10 @@ export class HomeComponent {
   BmiUnitInImperial = "lbs/in"
   bmiPrefix = "BMI = ";
   weightSuggestion = "";
-  // showImperialForm = false;
-  convertHeight: number | undefined;
+  // convertHeight: number | undefined;
+  //for ng model
+  selectedWeight = 'kg';
+  selectedHeight = 'cm';
 
   constructor() {
   }
@@ -58,14 +60,32 @@ export class HomeComponent {
 
       //  formula for calculating BMI (with kg and m)
       this.BMI = this.bmiForm.value.weight / Math.pow(this.finalHeight, 2);
-    } else {
-      //  formula for calculating BMI (with pound and inch)
+    }
+    else if (this.bmiForm.value.weightType == "kg" && this.bmiForm.value.heightType == "inch") {
+      // converting input height from inch to m
+      this.finalHeight = this.bmiForm.value.height * 0.0254;
+
+      //  formula for calculating BMI (with kg and m)
+      this.BMI = this.bmiForm.value.weight / Math.pow(this.finalHeight, 2);
+    }
+    else if (this.bmiForm.value.weightType == "lbs" && this.bmiForm.value.heightType == "cm") {
+      // converting input height from cm to inch
+      this.finalHeight = this.bmiForm.value.height / 2.54;
+
+      //  formula for calculating BMI (with lbs and inch)
+      this.BMI = (this.bmiForm.value.weight / Math.pow(this.finalHeight, 2)) * 703;
+    }
+    else if (this.bmiForm.value.weightType == "lbs" && this.bmiForm.value.heightType == "inch") {
+
+      //  formula for calculating BMI (with lbs and inch)
       this.BMI = (this.bmiForm.value.weight / Math.pow(this.bmiForm.value.height, 2)) * 703;
     }
+
     // call to show result of BMI categories
     this.showBmiCategories();
   }
 
+  // function to suggest weight range for healthy bmi
   suggestNormalWeight(): void {
 
     this.minNormalWeight = Math.round(18.5 * Math.pow(this.finalHeight, 2));
